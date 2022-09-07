0235aaa4be63cd3673366a412d4a721b39ff01fe
Nathanmsaxe <59581764+Nathanmsaxe@users.noreply.github.com>
2022-09-07 14:41:38 -0700

    Create Student.hpp

---
+++ Student.hpp
@@ -0,0 +1,24 @@
+/*
+ * Student.hpp
+ *
+ *  Created on: Sep 7, 2022
+ *      Author: Nathan Saxe
+ */
+
+#ifndef STUDENT_HPP_
+#define STUDENT_HPP_
+
+#include <string>
+
+using namespace std;
+
+struct Student{
+	string data;
+
+	int key;
+};
+
+
+
+
+#endif /* STUDENT_HPP_ */
