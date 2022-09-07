8fd35c68c892ca484c66277d0719101c97ee62bf
Nathanmsaxe <59581764+Nathanmsaxe@users.noreply.github.com>
2022-09-07 14:41:20 -0700

    Create LL.h

---
+++ LL.h
@@ -0,0 +1,34 @@
+/*
+ * LL.h
+ *
+ *  Created on: Sep 7, 2022
+ *      Author: Nathan Saxe
+ */
+
+#ifndef LL_H_
+#define LL_H_
+
+#include "Student.hpp"
+
+struct LLnode{
+
+	LLnode * fwdPtr;
+
+	Student theData;
+};
+template<class Data>
+class LL {
+private:
+	LLnode * fwdPtr;
+public:
+	LL();
+	void push_front(Data);
+	void push_back(Data);
+	int list_length();
+	Student retrieve_front();
+	Student retrieve_back();
+	void display_list();
+};
+
+#endif /* LL_H_ */
+
