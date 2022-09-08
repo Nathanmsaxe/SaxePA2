1c5d89ab00242e3c5066dc7beae73f71ce58651f
Nathanmsaxe <59581764+Nathanmsaxe@users.noreply.github.com>
2022-09-07 17:44:13 -0700

    Update LL.cpp

--- LL.cpp
+++ LL.cpp
@@ -1,10 +1,3 @@
-/*
- * LL.cpp
- *
- *  Created on: Sep 7, 2022
- *      Author: Nathan Saxe
- */
-
 #include "LL.h"
 #include <iostream>
 using namespace std;
@@ -80,4 +73,47 @@ void LL<Data>::display_list(){
 	else{
 		cout << "Empty List" << endl;
 	}
+}
+template<class Data>
+void LL<Data>::destroy_list(){
+	while(this->fwdPtr!=nullptr){
+		this->fwdPtr = this->fwdPtr->fwdPtr;
+		delete this;
+	}
+}
+template<class Data>
+Data LL<Data>::search_list(int targetKey){
+	while(this->fwdPtr!=nullptr){
+		if(this->fwdPtr->theData.key == targetKey){
+			Student temp;
+			temp.data = this->fwdPtr->theData.data;
+			temp.key = this->fwdPtr->theData.key;
+			return temp;
+		}
+		else{
+			Student temp;
+			temp.data = "";
+			temp.key = 0;
+			return temp;
+		}
+	}
+	Student temp;
+	temp.data = "";
+	temp.key = 0;
+	return temp;
+}
+template<class Data>
+bool LL<Data>::delete_node(int targetKey){
+	while(this->fwdPtr!=nullptr){
+		if(this->fwdPtr->fwdPtr->theData.key == targetKey){
+			LLnode * tempptr = this->fwdPtr->fwdPtr;
+			delete this->fwdPtr->fwdPtr;
+			this->fwdPtr = tempptr;
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	return false;
 }
